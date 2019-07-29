54d9ff9a183de2eb514af67e2d4cd3688644920c
josh <48034855+JDJoffe@users.noreply.github.com>
2019-07-29 16:07:19 +1000

    move ball

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    public float movementSped = 15f;
+    public Joystick joystick;
+    public GameObject sphere;
+    public Vector2 vecc;
+    private CharacterController characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = this.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vecc = new Vector2(joystick.Horizontal,  joystick.Vertical);
+        vecc = transform.TransformDirection(vecc);
+        vecc *= movementSped;
+
+        characterController.Move(vecc * Time.deltaTime);
+
+    }
+   
+
+    
+    
+}
